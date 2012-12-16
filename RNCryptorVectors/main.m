4806ebf54fc9bc10047ca2de827327749fa52e01
Rob Napier <robnapier@gmail.com>
2012-12-16 13:56:15 -0500

    Start of vectors

---
+++ RNCryptorVectors/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RNCryptorVectors
+//
+//  Created by Rob Napier on 12/16/12.
+//  Copyright (c) 2012 Rob Napier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
