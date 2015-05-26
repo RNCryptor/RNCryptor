f0e5d7bd17cd2f1a8a4bf808cb96533598baefb5
Daniel Brooks <daniel_brooks@dabsquared.com>
2015-05-25 21:31:34 -0700

    Adding modulemap

--- RNCryptor iOS/RNCryptor iOS.h
+++ RNCryptor iOS/RNCryptor iOS.h
@@ -14,7 +14,7 @@ FOUNDATION_EXPORT double RNCryptor_iOSVersionNumber;
 //! Project version string for RNCryptor iOS.
 FOUNDATION_EXPORT const unsigned char RNCryptor_iOSVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <RNCryptor_iOS/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <RNCryptor/PublicHeader.h>
 
 #import <RNCryptor/RNCryptor.h>
 #import <RNCryptor/RNDecryptor.h>
