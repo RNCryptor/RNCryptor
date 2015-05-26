1fd2311aca3ef6a33653d680c718c932ee86e4ec
Daniel Brooks <daniel_brooks@dabsquared.com>
2015-05-25 21:04:54 -0700

    Addj\ing iOS Framework

---
+++ RNCryptor iOS/RNCryptor iOS.h
@@ -0,0 +1,22 @@
+//
+//  RNCryptor iOS.h
+//  RNCryptor iOS
+//
+//  Created by Daniel Brooks on 5/25/15.
+//  Copyright (c) 2015 Rob Napier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RNCryptor iOS.
+FOUNDATION_EXPORT double RNCryptor_iOSVersionNumber;
+
+//! Project version string for RNCryptor iOS.
+FOUNDATION_EXPORT const unsigned char RNCryptor_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNCryptor_iOS/PublicHeader.h>
+
+#import <RNCryptor_iOS/RNCryptor.h>
+#import <RNCryptor_iOS/RNDecryptor.h>
+#import <RNCryptor_iOS/RNEncryptor.h>
+#import <RNCryptor_iOS/RNCryptorEngine.h>
