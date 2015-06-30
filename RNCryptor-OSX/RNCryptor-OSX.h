f6ac4640cfa9696a21a92c796af913fd0674e05d
Rob Napier <robnapier@gmail.com>
2015-06-30 15:55:37 -0400

    Add OS X

---
+++ RNCryptor-OSX/RNCryptor-OSX.h
@@ -0,0 +1,19 @@
+//
+//  RNCryptor-OSX.h
+//  RNCryptor-OSX
+//
+//  Created by Rob Napier on 6/30/15.
+//  Copyright © 2015 Rob Napier. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RNCryptor-OSX.
+FOUNDATION_EXPORT double RNCryptor_OSXVersionNumber;
+
+//! Project version string for RNCryptor-OSX.
+FOUNDATION_EXPORT const unsigned char RNCryptor_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNCryptor_OSX/PublicHeader.h>
+
+
