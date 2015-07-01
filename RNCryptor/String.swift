e89b0e3025310a5dec9a6c8cd374bba558091c10
Rob Napier <robnapier@gmail.com>
2015-07-01 08:59:06 -0400

    Add string helper

---
+++ RNCryptor/String.swift
@@ -0,0 +1,15 @@
+//
+//  String.swift
+//  RNCryptor
+//
+//  Created by Rob Napier on 7/1/15.
+//  Copyright © 2015 Rob Napier. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    init?(UTF8String: UnsafePointer<UInt8>) {
+        self.init(UnsafePointer<Int8>(UTF8String))
+    }
+}
