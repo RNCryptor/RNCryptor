b2fe098d898a7572fee33b7c53c5089c860cf67c
Jeff Kelley <SlaunchaMan@gmail.com>
2016-11-15 14:48:49 -0500

    Add watchOS and tvOS support.

---
+++ RNCryptor watchOS/RNCryptor watchOS.h
@@ -0,0 +1,22 @@
+//
+//  RNCryptor watchOS.h
+//  RNCryptor watchOS
+//
+//  Created by Jeff Kelley on 11/15/16.
+//  Copyright © 2016 Rob Napier. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for RNCryptor watchOS.
+FOUNDATION_EXPORT double RNCryptor_watchOSVersionNumber;
+
+//! Project version string for RNCryptor watchOS.
+FOUNDATION_EXPORT const unsigned char RNCryptor_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNCryptor/PublicHeader.h>
+
+#import <RNCryptor/RNCryptor.h>
+#import <RNCryptor/RNDecryptor.h>
+#import <RNCryptor/RNEncryptor.h>
+#import <RNCryptor/RNCryptorEngine.h>
